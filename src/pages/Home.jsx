3ab43d36ca94d0ce0075a337cc553a99dc8f7b64
Fortexfreddie <106601096+Fortexfreddie@users.noreply.github.com>
2025-07-05 13:59:46 +0100

    Responsive navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,12 @@ import HeroSection from "../components/sections/HeroSection";
 const Home = () => {
     return (  
         <div className="flex flex-col min-h-screen bg-[#272727]">
-            <Navbar />
             <div className="flex-grow">
-                <div className="container mx-auto p-4">
-                    <HeroSection />
+                <div className="container mx-auto">
+                    <Navbar />
+                    <div className="p-4">
+                        <HeroSection />
+                    </div>
                 </div>
             </div>
         </div>
