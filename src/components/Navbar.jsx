82425bf5f9fb86087f9b5735c8f6c03e72c2b272
Fortexfreddie <106601096+Fortexfreddie@users.noreply.github.com>
2025-07-05 13:50:23 +0100

    Added the image carousel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import TopBanner from "./TopBanner";
 import NavDropdown from "./NavDropdown";
 import Searchbar from "./SearchBar";
-import { FaBars, FaSearch } from 'react-icons/fa';
+import { FaBars, FaSearch, FaShoppingCart } from 'react-icons/fa';
 import { useState } from 'react';
 
 const Navbar = () => {
     const [showSearch, setShowSearch] = useState(false);
 
     return (  
-        <nav className="flex flex-col bg-transparent shadow-sm p-2">
+        <nav className="flex flex-col bg-transparent shadow-sm py-2 px-4">
             <div className="z-20 flex items-center justify-between py-2 px-6 bg-[rgba(255,255,255,0.08)] backdrop-blur-md border border-[rgba(255,255,255,0.15)] shadow-md rounded-xl">
                 <span className="lg:hidden text-[#D4AA7D] cursor-pointer"><FaBars /></span>
                 <TopBanner />
@@ -17,8 +17,11 @@ const Navbar = () => {
             <div className='flex items-center justify-center lg:justify-between gap-6 py-4 px-10'>
                 <h1 className='text-4xl lg:text-5xl text-[#D4AA7D] font-bold uppercase cursor-pointer'>Tr<span className='inline-block animate-bounce'>oo</span>pi</h1>
                 <span onClick={() => setShowSearch(!showSearch)} className="lg:hidden text-[#D4AA7D] bg-[rgba(255,255,255,0.08)] hover:bg-[rgba(255,255,255,0.2)] backdrop-blur-md border border-[rgba(255,255,255,0.15)] shadow-md rounded-full p-2 cursor-pointer transition-colors duration-200"><FaSearch /></span>
-                <div className="w-full max-w-xl hidden lg:block">
-                    <Searchbar />
+                <div className="flex items-center gap-4 max-w-lg">
+                    <div className="w-md hidden lg:block">
+                        <Searchbar />
+                    </div>
+                    <span className="hidden lg:block bg-[rgba(255,255,255,0.08)] hover:bg-[rgba(255,255,255,0.05)] active:bg-[rgba(255,255,255,0.05)] backdrop-blur-md border border-[rgba(255,255,255,0.15)] shadow-lg rounded-full p-2 text-[#D4AA7D] hover:text-[#c99660] active:text-[#c99660] cursor-pointer transition-colors duration-200"><FaShoppingCart className="h-6 w-6" /></span>
                 </div>
             </div>
             <div className={`block lg:hidden transition-all duration-500 px-6 ${showSearch ? 'opacity-100 translate-y-0 max-h-[200px] mt-4' : 'opacity-0 -translate-y-2 max-h-0'} `}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import Navbar from "../components/Navbar";
+import HeroSection from "../components/sections/HeroSection";
 
 const Home = () => {
     return (  
         <div className="flex flex-col min-h-screen bg-[#272727]">
             <Navbar />
             <div className="flex-grow">
-                <div className="container mx-auto">
+                <div className="container mx-auto p-4">
+                    <div className="flex items-center gap-4 w-full">
+                        <div className="w-[20%]"></div>
+                        <div className="w-[60%]"><HeroSection /></div>
+                        <div className="w-[20%]"></div>
+                    </div>
                 </div>
             </div>
         </div>
