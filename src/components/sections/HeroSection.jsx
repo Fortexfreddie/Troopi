f34a26dfdd5982f5cefb5fe837edabc40a6ffbad
Fortexfreddie <106601096+Fortexfreddie@users.noreply.github.com>
2025-07-06 18:13:09 +0100

    UI edit

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
         <div className="flex gap-4 w-full">
             <div className="w-full lg:w-[70%]"><Carousel /></div>
             <div className="hidden lg:block w-[30%]">
-                <div className="grid grid-cols-1 gap-4 h-full py-4">
+                <div className="grid grid-cols-1 gap-4 h-full">
                     <motion.div 
                         whileHover={{
                             y: [-2, -10, 3, -5, 4, -3, 0], // bounce keyframes
@@ -16,7 +16,7 @@ const HeroSection = () => {
                                 ease: "easeOut",
                             },
                         }}
-                        className="w-full h-full overflow-hidden backdrop-blur-md shadow-md rounded-md">
+                        className="w-full h-full overflow-hidden backdrop-blur-md shadow-md rounded-xl">
                         <img src={img} alt="" className="object-center object-cover h-full w-full" />
                     </motion.div>
                     <motion.div 
@@ -27,7 +27,7 @@ const HeroSection = () => {
                                 ease: "easeOut",
                             },
                         }}
-                        className="w-full h-full overflow-hidden backdrop-blur-md shadow-md rounded-md">
+                        className="w-full h-full overflow-hidden backdrop-blur-md shadow-md rounded-xl">
                         <img src={img} alt="" className="object-center object-cover h-full w-full" />
                     </motion.div>
                 </div>
