f9370323200d3856e91bc1afdbbfa25934c52d96
Frank Anthony <frankez478@gmail.com>
2025-07-11 14:35:18 -0700

    Implemented the Best sellers section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Backdrop from "./assets/backdrop.png";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={
+          <div
+            className="bg-cover bg-center h-screen flex justify-center items-center"
+            style={{ backgroundImage: `url(${Backdrop})` }}
+          >
+            <Login />
+          </div>
+        }
+      />
+      <Route path="/home" element={<Home />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,21 @@ import Navbar from "../components/Navbar";
 import HeroSection from "../components/sections/HeroSection";
 import Sidebar from "../components/Sidebar";
 import FeaturedSection from "../components/sections/FeaturedSection";
+import BestSellers from "../components/productsSection/BestSellers";
 
 const Home = () => {
     return (  
-        <div className="flex flex-col min-h-screen bg-[#272727]">
+        <div className="flex flex-col min-h-screen bg-[#272727] w-full overflow-hidden">
             <div className="flex-grow">
                 <div className="container mx-auto">
                     <Navbar />
                     <div className="px-4 py-8 bg-[#D4AA7D]/10">
                         <div className="flex gap-4">
                             <Sidebar />
-                            <div className="flex flex-col items-center gap-6">
+                            <div className="flex flex-col items-center gap-6 overflow-y-auto max-h-[calc(100vh-80px)]">
                                 <HeroSection />
                                 <FeaturedSection />
+                                <BestSellers />
                             </div>
                         </div>
                     </div>
