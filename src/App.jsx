9a2441cb38a72944b91df5525cda889b2b7954aa
Fortexfreddie <106601096+Fortexfreddie@users.noreply.github.com>
2025-07-12 14:28:05 +0100

    Overriding previous changes, added sticky to the nav and removed the scroller bar overflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Login from "./pages/auth/Login";
 import Backdrop from "./assets/backdrop.png";
 
 function App() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,14 @@ import BestSellers from "../components/productsSection/BestSellers";
 
 const Home = () => {
     return (  
-        <div className="flex flex-col min-h-screen bg-[#272727] w-full overflow-hidden">
+        <div className="flex flex-col min-h-screen bg-[#272727]">
             <div className="flex-grow">
                 <div className="container mx-auto">
                     <Navbar />
                     <div className="px-4 py-8 bg-[#D4AA7D]/10">
                         <div className="flex gap-4">
                             <Sidebar />
-                            <div className="flex flex-col items-center gap-6 overflow-y-auto max-h-[calc(100vh-80px)]">
+                            <div className="flex flex-col items-center gap-6">
                                 <HeroSection />
                                 <FeaturedSection />
                                 <BestSellers />
